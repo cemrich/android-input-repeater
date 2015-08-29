3872d5b148078e6e3ad2eb82a813ac44b206bd30
Christine Emrich <christine.emrich@web.de>
2015-08-29 17:37:38 +0200

    Basic working version.

--- android-input-repeater.js
+++ android-input-repeater.js
@@ -23,17 +23,75 @@ console.log("Devices detected:", devices);
 if (devices.length < 2) {
   console.error("There has to be more than one connected android device.");
 } else {
-  getInputEvents(devices[0], function (event) {
-      console.log(event);
+  var shellProcesses = getShellProcesses(devices);
+  devices.forEach(function (deviceId) {
+    mirrorInputEvents(deviceId, shellProcesses);
   });
 }
 
-function getInputEvents(deviceId, onEvent) {
+function mirrorInputEvents(sourceDeviceId, targetShellProcesses) {
+  var lastSentEvent;
+  var count = 0;
+  getInputEvents(sourceDeviceId, function (event) {
+    console.log(count++, sourceDeviceId, event);
+    if (isEqual(event, lastSentEvent)) return;
+    lastSentEvent = event;
+    Object.keys(targetShellProcesses).forEach(function (targetDeviceId) {
+      if (targetDeviceId !== sourceDeviceId) {
+        sendEvent(targetShellProcesses[targetDeviceId], event);
+      }
+    });
+  }, function (error) {
+      console.error(error);
+  });
+}
+
+function isEqual(event1, event2) {
+  return event1 && event2 &&
+    event1.type === event2.type &&
+    event1.params[0] === event2.params[0] &&
+    event1.params[1] === event2.params[1] &&
+    event1.params[2] === event2.params[2];
+}
+
+function getShellProcesses(deviceIds) {
+  var processes = {};
+  deviceIds.forEach(function (deviceId) {
+    processes[deviceId] = getShellProcess(deviceId);
+  });
+  return processes;
+}
+
+function getShellProcess(deviceId) {
+  var command = ADB_PATH + ' -s ' + deviceId + ' shell';
+  var shellProcess = spawn(command);
+  shellProcess.stdin.setEncoding('utf-8');
+  return shellProcess;
+}
+
+function sendEvent(shellProcess, event) {
+  var command = "sendevent /dev/input/event" + event.type + " " + event.params.join(" ") + '\n';
+  shellProcess.stdin.write(command);
+}
+
+function getInputEvents(deviceId, onEvent, onError) {
   var command = ADB_PATH + ' -s ' + deviceId + ' shell getevent';
-  var child = child_process.exec(command);
+  var child = spawn(command);
   child.stdout.on('data', function (data) {
     var eventStr = data.toString();
     parseEventString(eventStr, onEvent);
+  });
+  child.stdout.on('error', function (error) {
+    onError(error);
+  });
+  child.stdout.on('end', function (error) {
+    onError(error);
+  });
+  child.stdout.on('close', function (error) {
+    onError(error);
+  });
+  child.stderr.on('data', function (data) {
+    onError(data.toString());
   });
 }
 
@@ -45,6 +103,11 @@ function parseEventString(eventStr, onEvent) {
     eventObj.params = match.slice(2, 5).map(function (hex) { return parseInt(hex, 16) });
     onEvent(eventObj);
   }
+}
+
+function spawn(command) {
+  // TODO: this only works on windows machines, fix later
+  return child_process.spawn('cmd', ['/c', command], {env: process.env});
 }
 
 /**
